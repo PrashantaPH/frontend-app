f7e74178fb108845784ddf7a6b58dcc29e8ebb29
PrashantaPH <105696439+PrashantaPH@users.noreply.github.com>
2024-05-05 17:49:23 +0530

    Api path has been changeed

--- src/service/EmployeeService.jsx
+++ src/service/EmployeeService.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = "http://localhost:8080/api/employees";
+// const REST_API_BASE_URL = "http://localhost:8080/api/employees";
+const REST_API_BASE_URL = "https://ems-backend-me76.onrender.com/api/employees";
 
 export const listEmployees = () => {
     return axios.get(REST_API_BASE_URL);
